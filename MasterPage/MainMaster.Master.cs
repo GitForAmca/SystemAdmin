82225f53fa4fc8fae30bd1eecbde994a8070fb7c
Mohd Owais Ansari <it1@amca.ae>
2024-07-11 12:00:02 +0530

    files uploaded

---
+++ MasterPage/MainMaster.Master.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SystemAdmin.MasterPage
+{
+    public partial class MainMaster : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserAutoId"] == null)
+            {
+                Response.Redirect("~/Default.aspx", true);
+            }
+            if (!Page.IsPostBack)
+            {
+                lblUsername.Text = Session["UserName"].ToString();
+            }
+        }
+    }
+}
